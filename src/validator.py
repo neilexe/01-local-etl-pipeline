51c10b42dad7d1afb4d5f7c17a8f7a60296f384a
neilexe <neilbanerjee@Neils-MacBook-Pro.local>
2026-07-09 12:39:15 +0530

    Add negative age validation

--- src/validator.py
+++ src/validator.py
@@ -6,6 +6,7 @@ def validate_data(clean_df):
     working_df["validation_errors"] = pd.NA
     missing_customer_id = working_df["customer_id"].isna()
     duplicate_customer_id = working_df["customer_id"].duplicated(keep=False)
+    negative_age = working_df["age"] < 0
 
     # working_df.loc[missing_customer_id,"validation_errors"]="Customer ID is missing"
     working_df.loc[missing_customer_id,"validation_errors"] = (
@@ -17,6 +18,12 @@ def validate_data(clean_df):
     working_df.loc[duplicate_customer_id, "validation_errors"] = (
         working_df.loc[duplicate_customer_id, "validation_errors"].apply(
             lambda error : append_error(error, "Duplicate Customer ID")
+        )
+    )
+
+    working_df.loc[negative_age, "validation_errors"] = (
+        working_df.loc[negative_age, "validation_errors"].apply(
+            lambda error : append_error(error, "Age cannot be negative")
         )
     )
     return working_df
